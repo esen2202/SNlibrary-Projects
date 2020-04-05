a0238b3fc9a9b170797134de82f66a44445a460d
erkan esen <erkanesen2202@hotmail.com>
2020-04-05 15:05:23 +0300

    Add project files.

---
+++ SN.Library/Entities/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SN.Library.Entities
+{
+    public interface IEntity
+    {
+    }
+}
---
+++ SN.NetSet.Entities/Concrete/Network/NetConfig.cs
@@ -0,0 +1,18 @@
+﻿using SN.Library.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SN.NetSet.Entities.Concrete.Network
+{
+    public class NetConfig : IEntity
+    {
+        public int NetConfigId { get; set; }
+        public string ConfigName { get; set; }
+        public string IpAddress { get; set; }
+        public string Gateway { get; set; }
+        public string SubnetMask { get; set; }
+    }
+}
